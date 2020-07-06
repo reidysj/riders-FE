5937dd72d52d7fc2585a505b8704e65243dc24e6
unknown <sara.reidy.21@gmail.com>
2020-07-06 09:52:17 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import Login from "./login";
+import Home from './home'
 
 function App() {
   return (
     <ThemeProvider>
       <CSSReset />
+      <Route exact path='/'>
+        <Home />
+      </Route>
       <Route path="/login">
         <Login />
       </Route>
